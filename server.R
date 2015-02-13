ba555fbdd600879d064ee6f115e8283d72648e11
Kirill Sevastyanenko <kirill.sevastyanenko@avantcredit.com>
2015-02-13 12:48:01 -0600

    only run fix

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
 # options(repos = c(CRAN = "http://cran.rstudio.com"))
 # install.packages(c("devtools", "httpuv", "jsonlite"))
-# devtools::install_github("robertzk/microserver")
+devtools::install_github("robertzk/microserver")
 microserver::run_server(list("/ping" = function(p, q) c("pong")), 3464)
