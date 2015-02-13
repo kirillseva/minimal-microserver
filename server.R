4a2259b53d8f1e63eee2c18f46efd3957251c53b
Kirill Sevastyanenko <kirill.sevastyanenko@avantcredit.com>
2015-02-13 11:29:19 -0600

    will it work?

--- server.R
+++ server.R
@@ -1,2 +1,4 @@
-library(microserver)
+if (!require('packrat')) install.packages('packrat')
+packrat::restore()
+packrat::install_github(microserver)
 run_server(list("/ping" = function(p, q) c("pong")), 3464)
