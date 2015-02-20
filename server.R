70bdc5e18377bb4d59176f439d669b6aa984c6e2
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-02-20 16:31:33 -0600

    Update server.R

--- server.R
+++ server.R
@@ -3,4 +3,6 @@ interactive <- function(...) TRUE
 library(methods)
 library(s3mpi)
 routes <- s3read(Sys.getenv("MICROSERVER_ROUTES"))
+cat("loaded the following routes\n")
+print(routes)
 microserver::run_server(routes, 8101)
